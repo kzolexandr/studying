c54a11b34bdf386cf69f26a916288358132244a8
kzolexandr <58478409+kzolexandr@users.noreply.github.com>
2021-03-15 19:48:58 +0200

    menu

--- menu/menu.cpp
+++ menu/menu.cpp
@@ -2,21 +2,48 @@
 using namespace std;
 
 struct Element {
-    int Data;
+    int data;
     Element *next;
 };
 
-void reversed(int *arr, int length) {
+void printStack(Element *top) {
+	Element *element = top;
+	while (element) {
+		printf_s("%i ", element->data);
+		element = element->next;
+	}
+}
+
+void pushElement(Element **top, int value) {
+	Element *element;
+	element = new Element;
+	element->data = value;
+	if (top == NULL) {
+		*top = element;
+	}
+	else {
+		element->next = *top;
+		*top = element;
+	}
+}
+
+void reverseStack(Element*top, int length) {
+    int i;
+    int *arr = new int[length];
     int *temp = new int[length];
-    for (int k = 0; k < length; k++) {
-        temp[k] = arr[k];
-    }
+    Element *element = top;
+        while (element) {
+            temp[i] = element->data;
+            element = element->next;
+            i++;
+        }
     for (int m = 0; m < length; m++) {
-        arr[m] = temp[(length - 1) - m];
+       arr[m] = temp[(length - 1) - m];
+       cout << arr[m] << " ";
     }
 }
 
-void menu() {
+void menuPrint() {
     system("cls");
     cout << "Choose variant\n" << endl;
     cout << "1. Insert length\n"
@@ -28,52 +55,44 @@ void menu() {
 }
 
 int main() {
+    Element *top = NULL;
     int length;
-    int var;
-    int *arr = new int [length];
+    int variant;
     do {
-        menu();
-        cin >> var;
-    switch (var) {
-        case 1:
-            cout << "Insert your length..." << endl;
-            cin >> length;
-            break;
-        case 2:
-            cout << "Insert your values..." << endl;
-            for (int i = 0; i < length; i++) {
-            int v;
-            cin >> v;
-            Element *ptr = new Element;
-            ptr->Data = v;
-            arr[i] = ptr->Data;
-            }
-            break;
-        case 3:
-            cout << "Showing stack..." << endl;
-            for (int j = 0; j < length; j++) {
-            cout << arr[j] <<' ';
-            }
-            cout << endl;
-            break;
-        case 4:
-            cout << "Reversing stack..." << endl;
-            reversed(arr, length);
-            for (int j = 0; j < length; j++) {
-            cout << arr[j] <<' ';
-            }
-            break;
-        case 5:
-            cout << "Exiting..." << endl;
-            exit(EXIT_SUCCESS);
-            break;
-        default:
-            cerr << "You pick the wrong item, fool!" << endl;
-            exit(EXIT_FAILURE);
-    }
-    if (var <= 5) {
-        system ("pause");
-    }
-    }  while (var <= 5);
+        menuPrint();
+        cin >> variant;
+        switch (variant) {
+            case 1:
+                cout << "Insert length..." << endl;
+                cin >> length;
+                break;
+            case 2:
+                cout << "Insert your values..." << endl;
+                for (int i = 0; i < length; i++) {
+                    int value;
+                    cin >> value;
+                    pushElement(&top, value);
+                }
+                break;
+            case 3:
+                cout << "Showing stack..." << endl;
+                printStack(top);
+                break;
+            case 4:
+                cout << "Reversing stack..." << endl;
+                reverseStack(top, length);
+                break;
+            case 5:
+                cout << "Exiting..." << endl;
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                cerr << "You pick the wrong item, fool!" << endl;
+                exit(EXIT_FAILURE);
+        }
+        if (variant <= 5) {
+            system ("pause");
+        }
+    } while (variant <= 5);
     return 0;
 }
